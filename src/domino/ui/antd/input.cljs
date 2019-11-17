443230d1c16866ea71fd201dd743f9b3b6aea79e
Nikola Peric <nik@nikperic.com>
2019-11-17 10:45:00 -0500

    Input for testing

--- src/domino/ui/antd/input.cljs
+++ src/domino/ui/antd/input.cljs
@@ -1,12 +1,17 @@
 (ns domino.ui.antd.input
   (:require
     [syn-antd.input]
+    [re-frame.core :as rf]
+    [domino.ui.core :as core]
     [domino.ui.component]
     [syn-antd.reagent-utils]
-))
+    ))
 
-(defmethod domino.ui.component/component :input [opts]
-  [syn-antd.input/input opts])
+(defmethod domino.ui.component/component :input [{:keys [id disabled value on-change]
+                                                  :as   opts}]
+  [syn-antd.input/input (assoc opts :disabled (or disabled @(rf/subscribe [::core/component id]))
+                                    :value (or value @(rf/subscribe [::core/id id]))
+                                    :on-change (or on-change #(rf/dispatch [::core/id id (-> % .-target .-value)])))])
 
 (defmethod domino.ui.component/component :input-group [opts]
   [syn-antd.input/input-group opts])
